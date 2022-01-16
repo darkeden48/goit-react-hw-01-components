c4b1d614408228da7c3541069ff1ad2ec9186592
darkeden48 <dzalex72@list.ru>
2022-01-16 09:47:27 +0200

    ss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import user from "./user.json";
+import data from "./data.json";
 import Profile from "./components/Profile";
+import Statistics from "./components/Statistics";
 
 export default function App() {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    (
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    ),
+    (<Statistics title="Upload stats" stats={data} />),
+    (<Statistics stats={data} />)
   );
 }
